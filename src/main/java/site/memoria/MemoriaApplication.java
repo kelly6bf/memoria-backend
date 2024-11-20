6b81673da65c808752468cb09b67b14c5552b41c
kelly6bf <kelly6bf@gmail.com>
2024-11-20 20:09:14 +0900

    Init Project

---
+++ src/main/java/site/memoria/MemoriaApplication.java
@@ -0,0 +1,13 @@
+package site.memoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoriaApplication.class, args);
+    }
+
+}
